acac1416205fcff7c7851919ba2a17a69a8edeb7
vash <phyrox.vash512@gmail.com>
2024-07-04 02:20:50 -0600

    admin para news con selector de scraping de links

---
+++ news/admin.py
@@ -0,0 +1,93 @@
+from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import path
+
+from .models import Link, News, SearchQuery, SourceMethod
+
+
+class LinkInline(admin.StackedInline):
+    show_change_link = True
+    model = Link
+    extra = 0
+    fields = ('gnews_url', 'real_url', 'title', 'source')
+
+
+@admin.register(SearchQuery)
+class SearchQueryAdmin(admin.ModelAdmin):
+    list_display = ('query', 'created_at')
+    inlines = [LinkInline]
+
+
+def apply_selected_method(modeladmin, request, queryset):
+    if 'apply' in request.POST:
+        selected_method_id = request.POST['source_method']
+        selected_method = SourceMethod.objects.get(id=selected_method_id)
+
+        for link in queryset:
+            selected_method.news_by_link(link)
+            print(f'Aplicando {selected_method} a {link}')
+
+        modeladmin.message_user(request, "Método aplicado exitosamente")
+        return redirect(request.get_full_path())
+
+    print("SourceMethod.objects.all()", SourceMethod.objects.all().count())
+
+    return render(request, 'admin/apply_selected_method.html', context={
+
+        'links': queryset,
+        'source_methods': SourceMethod.objects.all(),
+        'action_checkbox_name': admin.helpers.ACTION_CHECKBOX_NAME,
+    })
+
+
+apply_selected_method.short_description = "Scrapear con método seleccionado"
+
+
+class NewsInline(admin.StackedInline):
+    model = News
+    extra = 0
+    fields = ('title', 'subtitle', 'content', "source_method")
+    readonly_fields = ('title', 'subtitle', 'content', "source_method")
+
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    actions = [apply_selected_method]
+    list_display = ('url_display', 'title', 'source', "news_count")
+    list_filter = ('source',)
+    search_fields = ('title', 'source')
+    inlines = [NewsInline]
+
+    def url_display(self, obj: Link):
+        return obj.real_url or obj.gnews_url[:50]
+
+    def news_count(self, obj: Link):
+        return obj.news.count()
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('apply-selected-method/',
+                 self.admin_site.admin_view(apply_selected_method))
+        ]
+        return custom_urls + urls
+
+    def apply_selected_method_view(self, request):
+        if request.method == 'POST':
+            selected = request.POST.getlist(admin.helpers.ACTION_CHECKBOX_NAME)
+            queryset = Link.objects.filter(pk__in=selected)
+            return apply_selected_method(self, request, queryset)
+        else:
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@admin.register(SourceMethod)
+class SourceMethodAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'title_tag', 'subtitle_tag', 'content_tag')
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle', 'content')
+    raw_id_fields = ('link', 'source_method')
